e4fab1f003340ac9def5805c5fb3e424a042aa95
jheart-vic <adebowalevictorjheart.com>
2022-07-12 14:44:59 +0100

    Connect react to redux

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { useDispatch } from 'react-redux/es/exports';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
 import './Book.css';
 import { categories } from '../redux/Categories/categories';
 
 const Category = () => {
+  const category = useSelector((state) => state.CategoryReducer);
   const dispatch = useDispatch();
   return (
     <div>
       <>
-        <h1>checking...</h1>
+        <h1>{category}</h1>
         <button type="button" className="updateBtn" onClick={() => dispatch(categories())}>Check status</button>
       </>
     </div>
